2eaf02571a15608ac7bdf69320650eb8e4f1422b
jbillote <jbillote@gmail.com>
2024-11-07 15:31:29 -0800

    Update servant models

--- src/models/servant.ts
+++ src/models/servant.ts
@@ -1,27 +1,10 @@
 import { t } from 'elysia'
-import { skill } from './skill'
-import { material } from './material'
 
 const servant = t.Object({
     id: t.Number(),
     name: t.String(),
     classIcon: t.String(),
-    icon: t.String(),
-    portraits: t.Array(t.String()),
-    skills: t.Array(skill),
-    appends: t.Array(skill),
-    ascensionMaterials: t.Array(t.Object({
-        materials: t.Array(material),
-        qp: t.Numeric()
-    })),
-    skillMaterials: t.Array(t.Object({
-        materials: t.Array(material),
-        qp: t.Numeric()
-    })),
-    appendMaterials: t.Array(t.Object({
-        materials: t.Array(material),
-        qp: t.Numeric()
-    }))
+    icon: t.String()
 })
 
 export { servant }
---
+++ src/models/servantDetails.ts
@@ -0,0 +1,27 @@
+import { t } from 'elysia'
+import { skill } from './skill'
+import { material } from './material'
+
+const servantDetails = t.Object({
+    id: t.Number(),
+    name: t.String(),
+    classIcon: t.String(),
+    icon: t.String(),
+    portraits: t.Array(t.String()),
+    skills: t.Array(skill),
+    appends: t.Array(skill),
+    ascensionMaterials: t.Array(t.Object({
+        materials: t.Array(material),
+        qp: t.Numeric()
+    })),
+    skillMaterials: t.Array(t.Object({
+        materials: t.Array(material),
+        qp: t.Numeric()
+    })),
+    appendMaterials: t.Array(t.Object({
+        materials: t.Array(material),
+        qp: t.Numeric()
+    }))
+})
+
+export { servantDetails }

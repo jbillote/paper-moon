1f2ea9207071762a60406748eb232916e6f422c6
jbillote <jbillote@gmail.com>
2024-11-19 20:51:27 -0800

    Update tsconfig and lock Elysia version

--- src/views/components/base.tsx
+++ src/views/components/base.tsx
@@ -1,24 +1,18 @@
-import { Html } from '@elysiajs/html'
-import type * as elements from 'typed-html'
+import * as elements from 'typed-html'
 
-const Base = (props: elements.Children) => {
-    return (
-        '<!DOCTYPE html>' +
-        (
-            <html lang="en" class="dark">
-                <head>
-                    <meta charset="UTF-8" />
-                    <meta
-                        name="viewport"
-                        content="width=device-width, inital-scale=1.0"
-                    />
-                    <title>Paper Moon</title>
-                    <link href="/public/globals.css" rel="stylesheet" /> 
-                </head>
-                <body>{props.children}</body>
-            </html>
-        )
-    )
-}
+const Base = ({ children }: elements.Children) => (
+    <html lang="en" class="dark">
+        <head>
+            <meta charset="UTF-8" />
+            <meta
+                name="viewport"
+                content="width=device-width, inital-scale=1.0"
+            />
+            <title>Paper Moon</title>
+            <link href="/public/globals.css" rel="stylesheet" /> 
+        </head>
+        <body>{children}</body>
+    </html>
+)
 
 export { Base }
--- src/views/index.tsx
+++ src/views/index.tsx
@@ -1,12 +1,13 @@
-import { html, Html } from '@elysiajs/html'
+import { html } from '@elysiajs/html'
 import { Elysia } from 'elysia'
 import { Base } from './components/base'
+import * as elements from 'typed-html'
 
 const views = new Elysia()
     .use(html())
     .get('/', () => (
         <Base>
-            <h1 class="h-screen flex items-center justify-center">
+            <h1 class='h-screen flex w-full items-center justify-center'>
                 P A P E R&nbsp;&nbsp;&nbsp;M O O N
             </h1>
         </Base>

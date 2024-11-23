4985c04275de8eeec4c3d06678e065aa1b0cf1fc
jbillote <jbillote@gmail.com>
2024-11-22 22:36:46 -0800

    Style placeholder text

--- src/views/index.tsx
+++ src/views/index.tsx
@@ -6,7 +6,7 @@ const views = new Elysia()
     .use(html())
     .get('/', () => (
         <Base>
-            <h1 class='flex w-full h-screen justify-center items-center'>
+            <h1 class='flex w-full h-screen justify-center items-center text-4xl font-bold'>
                 P A P E R&nbsp;&nbsp;&nbsp;M O O N
             </h1>
         </Base>

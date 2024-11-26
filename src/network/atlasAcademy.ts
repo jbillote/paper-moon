6846b35748ebf5b2138a49d8e9c0e3a09f242fe1
jbillote <jbillote@gmail.com>
2024-11-25 19:22:28 -0800

    Fix type safety warnings

--- src/network/atlasAcademy.ts
+++ src/network/atlasAcademy.ts
@@ -1,9 +1,11 @@
+// TODO: Implement more robust error checking for any network errors, JSON
+// parsing errors, and any potential type errors
 class AtlasAcademy {
     static async getServants(start: number, end: number): Promise<{[x:string]: any}[]> {
         // TODO: Cache this JSON file since it shouldn't change very often
         const url: string = "https://api.atlasacademy.io/export/JP/basic_servant_lang_en.json"
         const resp: Response = await fetch(url)
-        const respJson: object[] = await resp.json()
+        const respJson: {[x: string]: any}[] = await resp.json() as {[x: string]: any}[]
 
         return [...respJson.slice(start, end)]
     }
@@ -12,13 +14,13 @@ class AtlasAcademy {
         // TODO: Cache response for pagination
         const url: string = `https://api.atlasacademy.io/basic/JP/servant/search?name=${query}&lang=en`
         const resp: Response = await fetch(url)
-        return await resp.json()
+        return await resp.json() as {[x: string]: any}[]
     }
 
     static async getServant(id: number): Promise<{[x: string]: any}> {
         const url: string = `https://api.atlasacademy.io/nice/JP/servant/${id}?lang=en`
         const resp: Response = await fetch(url)
-        return await resp.json()
+        return await resp.json() as {[x: string]: any}[]
     }
 
     static classIconURL(classId: number, rarity: number): string {

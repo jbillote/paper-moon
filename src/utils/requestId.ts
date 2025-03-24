27e5441e5623798e4dd5610c3fea75e622b9947a
jbillote <jbillote@gmail.com>
2025-03-24 14:52:47 -0700

    Update request ID middleware

--- src/utils/requestId.ts
+++ src/utils/requestId.ts
@@ -1,9 +1,9 @@
 import { Elysia } from 'elysia'
 import { randomUUID } from 'crypto'
 
-const RequestID = new Elysia()
-    .onRequest(({ set }) => {
-        set.headers['X-Request-ID'] = randomUUID()
+const RequestID = new Elysia({ name: 'request-id', seed: 'header' })
+    .onRequest(({ set, request: { headers } }) => {
+        set.headers['X-Request-ID'] = headers.get('X-Request-ID') || randomUUID()
     })
     .derive({ as: 'global' }, ({ set }) => {
         return {
